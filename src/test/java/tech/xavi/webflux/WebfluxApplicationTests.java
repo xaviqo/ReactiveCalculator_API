2188affed937c32c3b94b2e9f462ccc6a46786cc
xaviqo <silent90s@gmail.com>
2022-10-05 23:47:17 +0200

    Spring Init

---
+++ src/test/java/tech/xavi/webflux/WebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.xavi.webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
